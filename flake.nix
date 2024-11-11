463e050544cb6370605cd88aeb4f8be060f0ed52
JohnFinn <dz4tune@gmail.com>
2024-11-12 00:28:35 +0100

    some chgs

--- flake.nix
+++ flake.nix
@@ -1,14 +1,43 @@
 {
   description = "A very basic flake";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+  inputs = {nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";};
 
-  outputs = { self, nixpkgs, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      devShells.${system}.default =
-        pkgs.mkShell { nativeBuildInputs = with pkgs; [ conan cmake ninja ]; };
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [conan cmake ninja];
     };
+
+    packages.${system} = {
+      default = pkgs.stdenv.mkDerivation {
+        name = "default";
+        src = ./conan_profile;
+
+        buildPhase = ''
+          echo default > $out
+        '';
+        phases = ["buildPhase"];
+      };
+      foo = pkgs.stdenv.mkDerivation {
+        name = "foobar";
+        src = ./conan_profile;
+
+        buildPhase = ''
+          echo foobar > $out
+        '';
+        phases = ["buildPhase"];
+      };
+    };
+
+    # conan install . --output-folder ./build --profile:build=./conan_profile --profile:host=./conan_profile --build=missing
+    # cmake --preset conan-release
+    # cmake --build build/
+  };
 }

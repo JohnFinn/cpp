35487fe5f68bed7ec4b500ed912b674f303b753b
JohnFinn <dz4tune@gmail.com>
2025-02-28 19:20:20 +0100

    basic parsing

--- main.cpp
+++ main.cpp
@@ -1,24 +1,19 @@
-#include "msg_stream_iterator.h"
+#include <iostream>
 #include <ranges>
-
-
-class MsgView : public std::ranges::view_interface<MsgView> {
-public:
-    MsgView(std::span<std::uint8_t> span) : _span(span) {}
-
-    auto begin() {
-        return msg_stream_iter(_span.data(), _span.size());
-    }
-
-    auto end() {
-        return msg_stream_iter(_span.data() + _span.size(), 0);
-    }
-
-private:
-    std::span<std::uint8_t> _span;
-};
+#include <sstream>
+#include <string>
+#include <vector>
 
 int main() {
+  std::vector<std::pair<int, int>> pairs;
+  std::string input(std::istreambuf_iterator<char>{std::cin},
+                    {}); // Read entire stdin as a string
 
-    return 0;
+  for (auto line :
+       input | std::views::split('\n') |
+           std::views::filter([](auto line) { return *line.begin() != 'c'; })) {
+    std::cout << std::string{line.begin(), line.end()}
+              << '\n'; // Convert view to string
+  }
+  return 0;
 }

2de06df5fb7517e706549deaf6f95a972fcb6944
JohnFinn <dz4tune@gmail.com>
2025-03-01 18:19:04 +0100

    more tests

--- test.cpp
+++ test.cpp
@@ -4,7 +4,26 @@
 #include <gtest/gtest.h>
 
 TEST(foo, bar) {
+  using namespace ::testing;
+  EXPECT_THAT(Graph(std::vector<Graph::Edge>{}).vertex_cover(),
+              UnorderedElementsAre());
   EXPECT_THAT(Graph(std::vector<Graph::Edge>{{1, 2}, {2, 3}, {4, 5}, {4, 6}})
                   .vertex_cover(),
-              ::testing::UnorderedElementsAre(2, 4));
+              UnorderedElementsAre(2, 4));
+  EXPECT_THAT(Graph(std::vector<Graph::Edge>{{1, 2}, {1, 3}, {1, 4}, {1, 5}})
+                  .vertex_cover(),
+              UnorderedElementsAre(1));
+  EXPECT_THAT(Graph(std::vector<Graph::Edge>{{2, 1}, {1, 3}, {4, 1}, {1, 5}})
+                  .vertex_cover(),
+              UnorderedElementsAre(1));
+  EXPECT_THAT(Graph(std::vector<Graph::Edge>{{1, 2},
+                                             {1, 3},
+                                             {1, 4},
+                                             {1, 5},
+                                             {10, 20},
+                                             {10, 30},
+                                             {10, 40},
+                                             {10, 50}})
+                  .vertex_cover(),
+              UnorderedElementsAre(1, 10));
 }

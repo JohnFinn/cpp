05459a851c332bb34f88ef741ecf8ebfc74bcd86
JohnFinn <dz4tune@gmail.com>
2025-03-04 21:08:47 +0100

    use clang

--- flake.nix
+++ flake.nix
@@ -1,29 +1,19 @@
 {
-  description = "A very basic flake";
-
   inputs = {nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";};
-
   outputs = {nixpkgs, ...}: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
-    nativeBuildInputs = with pkgs; [conan cmake ninja gtest flamegraph];
+    nativeBuildInputs = with pkgs; [
+      conan
+      cmake
+      ninja
+      flamegraph
+      libunwind # to make clang work
+      llvmPackages.clang-tools # https://github.com/NixOS/nixpkgs/issues/273875#issuecomment-2372118124
+    ];
   in {
-    devShells.${system}.default = pkgs.mkShell {
+    devShells.${system}.default = pkgs.mkShell.override {stdenv = pkgs.clangStdenv;} {
       inherit nativeBuildInputs;
-    };
-
-    packages.${system} = {
-      default = pkgs.stdenv.mkDerivation {
-        name = "foo";
-        src = ./.;
-        inherit nativeBuildInputs;
-
-        buildPhase = ''
-          cmake -S $src -B $out -G Ninja
-          ninja -C $out
-        '';
-        phases = ["buildPhase"];
-      };
     };
   };
 }

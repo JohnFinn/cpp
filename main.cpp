5700a39e8142faa070826b5bb03bbb497d34b3cf
JohnFinn <dz4tune@gmail.com>
2025-03-28 00:23:17 +0100

    implement timeout

--- main.cpp
+++ main.cpp
@@ -74,7 +74,7 @@ public:
 
   static std::optional<SubprocessHandle> fork() {
     if (int pid = ::fork(); pid > 0) {
-      return std::make_optional<SubprocessHandle>(pid);
+      return std::optional<SubprocessHandle>(pid);
     } else if (pid == 0) {
       return std::nullopt;
     } else {
@@ -84,6 +84,11 @@ public:
 
   SubprocessHandle(const SubprocessHandle&) = delete;
   SubprocessHandle& operator=(const SubprocessHandle&) = delete;
+  SubprocessHandle(SubprocessHandle&&) = delete;
+  SubprocessHandle& operator=(SubprocessHandle&&) = delete;
+
+  void kill() { ::kill(_pid, SIGTERM); }
+
   ~SubprocessHandle() { ::waitpid(_pid, nullptr, 0); }
 };
 
@@ -107,25 +112,43 @@ public:
       throw std::runtime_error("read failed");
     }
   }
+
   void write(std::span<const std::byte> span) {
     if (::write(write_fd(), span.data(), span.size_bytes()) !=
         span.size_bytes()) {
       throw std::runtime_error("write failed");
     }
   }
+
+  bool poll_read(std::chrono::milliseconds timeout) {
+    pollfd pfd{.fd = read_fd(), .events = POLLIN, .revents = 0};
+    if (auto ret = ::poll(&pfd, 1, timeout.count()); ret > 0) {
+      return true;
+    } else if (ret == 0) {
+      return false;
+    } else {
+      throw std::runtime_error("poll failed");
+    }
+  }
 };
 
 template <typename F>
   requires std::is_trivially_copyable_v<std::invoke_result_t<F>>
-const auto timeout(std::chrono::seconds duration, F f) {
+const std::optional<std::invoke_result_t<F>>
+timeout(std::chrono::seconds duration, F f) {
   pipe_fds p;
   using res_t = std::invoke_result_t<F>;
   if (auto subprocess = SubprocessHandle::fork(); subprocess.has_value()) {
     std::array<std::byte, sizeof(res_t)> buffer;
-    auto before = std::chrono::high_resolution_clock::now();
-    // TODO:: implement timeouting
-    p.read(buffer);
-    return std::bit_cast<res_t>(buffer);
+    const auto deadline = std::chrono::high_resolution_clock::now() + duration;
+    while (std::chrono::high_resolution_clock::now() < deadline) {
+      if (p.poll_read(std::chrono::milliseconds(10))) {
+        p.read(buffer);
+        return std::bit_cast<res_t>(buffer);
+      }
+    }
+    subprocess->kill();
+    return std::nullopt;
   } else {
     const auto result = f();
     const auto arr =
@@ -165,12 +188,14 @@ int main(int argc, char **argv) {
           const auto graph =
               parse_graph(std::string(std::istreambuf_iterator<char>{fin}, {}));
 
-          const auto time = timeout(std::chrono::seconds(10), [&graph] {
+          const auto time = timeout(std::chrono::seconds(3), [&graph] {
             return measure_time(
                        [&graph] { return graph.vertex_cover().size(); })
                 .first;
           });
-          std::cout << graph.edges().size() << ',' << time.count() << '\n';
+          std::cout << graph.edges().size() << ','
+                    << time.value_or(std::chrono::nanoseconds(-1)).count()
+                    << std::endl;
         });
         return 0;
       })
